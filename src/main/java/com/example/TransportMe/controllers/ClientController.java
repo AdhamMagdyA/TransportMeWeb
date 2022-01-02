0d0a38641b8b3e7200fde824f71f85ae2329d62d
AdhamMagdyA <adhammagdyz67@gmail.com>
2022-01-02 11:18:39 +0200

    created controllers

---
+++ src/main/java/com/example/TransportMe/controllers/ClientController.java
@@ -0,0 +1,7 @@
+package com.example.TransportMe.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ClientController {
+}
---
+++ src/main/java/com/example/TransportMe/controllers/DriverController.java
@@ -0,0 +1,8 @@
+package com.example.TransportMe.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DriverController {
+
+}
